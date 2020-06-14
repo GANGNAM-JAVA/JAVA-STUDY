dc0b811cdc5855cf0b844bb98eb351582dbad982
Incheol Jung <bluesky761@naver.com>
2020-06-14 20:09:04 +0900

    완주하지 못한 선수 문제 해결

--- src/Q1/incheol/Solution.java
+++ src/Q1/incheol/Solution.java
@@ -1,24 +1,32 @@
 package Q1.incheol;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "  ";
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        int i =0;      //a
-        for(i =0; i<completion.length;i++){
-            if(!participant[i].equals(completion[i])){
-                return participant[i];
-            }
-        }
-        return participant[i];
+        String answer = "";
+        HashMap<String, Integer> completionCountMap = new HashMap<String, Integer>();
+
+        for(String participantName : participant)
+            completionCountMap.put(participantName, completionCountMap.getOrDefault(participantName, 0) + 1);
+
+        for(String completionName : completion)
+            completionCountMap.put(completionName, completionCountMap.get(completionName) - 1);
+
+        answer = completionCountMap.entrySet()
+                .stream()
+                .filter(completionCount -> completionCount.getValue() != 0)
+                .findFirst()
+                .map(completionCount -> completionCount.getKey())
+                .orElse("");
+
+        return answer;
     }
 
     public static void main(String[] args) {
-        String[] participants = {"incheol","steve","andrew"};
-        String[] completions = {"incheol","steve","andrew2"};
+        String[] participants = {"mislav", "stanko", "mislav", "ana"};
+        String[] completions = {"stanko", "ana", "mislav"};
 
         Solution solution = new Solution();
         String participantName = solution.solution(participants, completions);
