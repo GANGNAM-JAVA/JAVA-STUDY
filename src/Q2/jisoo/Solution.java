d0caffa6bba89180b26a87c43d070a3f8f0626e4
JisooJang <ppuagirls@naver.com>
2020-06-18 14:00:41 +0900

    fix: change class name

--- src/Q2/jisoo/Solution.java
+++ src/Q2/jisoo/Solution.java
@@ -9,7 +9,7 @@ https://programmers.co.kr/learn/courses/30/lessons/42578
 스파이가 가진 의상들이 담긴 2차원 배열 clothes가 주어질 때
 서로 다른 옷의 조합의 수를 return 하도록 solution 함수를 작성해주세요.
  */
-public class Camouflage2 {
+public class Solution {
     public static void main(String[] args) {
         String[][] testcase1 = {
                 {"yellow_hat", "headgear"},
