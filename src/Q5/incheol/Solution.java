713b69136349f6b303dd8c38d723dc43522d8dcf
Incheol Jung <bluesky761@naver.com>
2020-07-12 16:27:58 +0900

    5번 문제 문제 해결

--- src/Q5/incheol/Solution.java
+++ src/Q5/incheol/Solution.java
@@ -6,13 +6,28 @@ import java.util.Stack;
  * @author Incheol Jung
  */
 public class Solution {
+    private enum  Backet{
+        OPEN('('),
+        CLOSE(')');
+
+        final private char character;
+
+        Backet(char character) {
+            this.character = character;
+        }
+
+        public char getValue(){
+            return this.character;
+        }
+    }
+
     public int solution(String arrangement) {
         int answer = 0;
         Stack<Integer> st = new Stack<>();
         for (int i = 0; i < arrangement.length(); i++) {
-            if (arrangement.charAt(i) == '(') {
+            if (arrangement.charAt(i) == Backet.OPEN.getValue()) {
                 st.push(i);
-            } else if (arrangement.charAt(i) == ')') {
+            } else if (arrangement.charAt(i) == Backet.CLOSE.getValue()) {
                 if (st.peek() + 1 == i) {
                     st.pop();
                     answer += st.size();
