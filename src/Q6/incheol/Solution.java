0f67a523a70139a935c04b3257fa12389083433f
Incheol Jung <bluesky761@naver.com>
2020-07-19 23:26:18 +0900

    단어 변환 문제 해결

--- src/Q6/incheol/Solution.java
+++ src/Q6/incheol/Solution.java
@@ -4,33 +4,46 @@ package Q6.incheol;
  * @author Incheol Jung
  */
 public class Solution {
-    int answer;
+    // 최단 단계를 클래스 전역 변수로 관리한다.
+    int answer = 0;
 
+    // 재귀 함수로 실행한다.
     public int solution(String begin, String target, String[] words) {
-        recursive(begin, target, 0, words, 0);
+        recursive(begin, target,0, words, 0, new boolean[words.length]);
         return answer;
     }
 
-    private void recursive(String begin, String target, int index, String[] words, int count){
+    /**
+     *
+     * @param begin 시작할 단어(인덱스가 변경할때마다 begin 단어가 변경됨)
+     * @param target 결과적으로 매칭해야 할 단어(변하지 않아야 함)
+     * @param index words의 index를 조율하기 위한 변수
+     * @param words words
+     * @param count 단계가 증가할 때마다 카운트를 증가 시킴
+     * @param visited "hit", "hhh" , new String[]{"hhh","hht"} 케이스 일 경우 처음부터 다시 돌아가서 조회할 경우 필요한 방문 표
+     */
+    void recursive(String begin, String target, int index,  String[] words, int count, boolean[] visited){
+        // begin을 이동하여 target과 일치할 경우 answer 값에 count를 넣어주는데 최솟값일 경우에만 answer 값을 변경해준다.
         if(begin.equals(target)) {
-            answer = answer > count ? count : answer;
-            return;
+            answer = answer == 0 ? count : answer > count ? count : answer;
         }
+        // 무한루프가 빠지지 않도록 index 값이 words 사이즈라면 재귀를 멈춘다.
+        if(index == words.length) return;
 
-        if(index == words.length-1) return;
-
-        for(int i=0; i <words.length; i++){
-            if(isChangeable(begin, words[index])){
-                recursive(begin, target, index + 1, words, count++);
-            }else {
-                recursive(words[index], target, index + 1, words, count);
+        for(int i=index; i < words.length; i++) {
+            if(isChangable(begin, words[i]) && !visited[i]) {
+                visited[i] = true;
+                recursive(words[i], target, 0, words, count+1, visited);
+                visited[i] = false;
+            } else{
+                recursive(begin, target, i + 1, words, count, visited);
             }
         }
     }
 
-    private boolean isChangeable(String begin, String target){
+    // 두 단어를 비교하여 한 단어만 다른지 확인하여 true, false 값을 내어준다.
+    private boolean isChangable(String begin, String target){
         int unMatchCount = 0;
-
         char[] beginCharArr = begin.toCharArray();
         char[] targetCharArr = target.toCharArray();
 
@@ -40,6 +53,7 @@ public class Solution {
                 if(unMatchCount > 1) return false;
             }
         }
+
         return unMatchCount == 1;
     }
 
@@ -47,43 +61,10 @@ public class Solution {
         Solution solution = new Solution();
         System.out.println(solution.solution(
             "hit",
-            "cog",
-            new String[]{"hot", "dot", "dog", "lot", "log", "cog"})
-        );
+            "hhh",
+            new String[]{"hhh","hht"}
+//            new String[]{"hot"}
+//            new String[]{"hot", "dot", "dog", "lot", "log", "cog"}
+        ));
     }
-
-
-//    public int solution(String begin, String target, String[] words) {
-//        int answer = 0;
-//        return recursiveSearch(begin, target, words, answer);
-//    }
-//
-//    private int recursiveSearch(String begin, String target, String[] words, int answer){
-//        for(String word : words){
-//            if(begin.equals(target)){
-//                return answer;
-//            }else {
-//                if(word == words[words.length-1]) return 0;
-//
-//                if(compareChar(begin,word)){
-//                    answer++;
-//                    return recursiveSearch(word, target, words, answer);
-//                }
-//            }
-//        }
-//        return 0;
-//    }
-//
-//    private boolean compareChar(String searchedString, String targetString){
-//        int cnt = 0;
-//        for (int i = 0; i < searchedString.length(); i++) {
-//            for (int j = 0; j < targetString.length(); j++) {
-//                if (searchedString.charAt(i) == targetString.charAt(j)) {
-//                    cnt++; // ont for character equal
-//                    if(cnt >= 2) return false;
-//                }
-//            }
-//        }
-//        return cnt == 1;
-//    }
 }
