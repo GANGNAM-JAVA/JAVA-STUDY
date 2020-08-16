b1a11fef759f11d41141dadb4f7e3de445c38f8d
leekyunghee <qkqhlkh@gmail.com>
2020-08-17 00:21:24 +0900

    Delete Unused cache

--- src/Q10/kyunghee/Solution.java
+++ src/Q10/kyunghee/Solution.java
@@ -1,23 +1,18 @@
-package problems;
-
 public class Solution {
-    private static int[][] directions = {
+    private int[][] directions = {
             {1, 0},  //상
             {-1, 0}, //하
             {0, -1}, //좌
             {0, 1}   //우
     };
 
-    public static int execute(char[][] grid) {
-        int yLen = grid.length;
-        int xLen = grid[0].length;
-        boolean[][] cache = new boolean[yLen][xLen];
+    public int execute(char[][] grid) {
         int count = 0;
         for (int y = 0; y < grid.length; y++) {
             for (int x = 0; x < grid[0].length; x++) {
-                if (grid[y][x] == '1' && cache[y][x] == false){
+                if (grid[y][x] == '1'){
                     count++;
-                    dfs(grid, y, x, cache);
+                    dfs(grid, y, x);
                 }
             }
         }
@@ -25,23 +20,21 @@ public class Solution {
         return count;
     }
 
-    private static int dfs(char[][] grid, int y, int x, boolean[][] cache) {
-        if (cache[y][x]) {
-            return 0;
+    private void dfs(char[][] grid, int y, int x) {
+        if (grid[y][x] == '2') {    //visited
+            return;
         }
 
-        int result = 0;
         for (int i = 0; i < directions.length; i++) {
             if (check(grid, y+directions[i][0], x+directions[i][1])) {
-                cache[y][x] = true;
-                dfs(grid, y+directions[i][0], x+directions[i][1], cache);
+                grid[y][x] = '2';
+                dfs(grid, y+directions[i][0], x+directions[i][1]);
             }
         }
-        return result;
     }
 
-    private static boolean check(char[][] grid, int y, int x) {
-        if (y < 0 || grid.length == y || x < 0 || grid[0].length == x) {
+    private boolean check(char[][] grid, int y, int x) {
+        if (y < 0 || grid.length == y || x < 0 || grid[0].length == x) { // edge
             return false;
         } else if (grid[y][x] == '0') { // water
             return false;
