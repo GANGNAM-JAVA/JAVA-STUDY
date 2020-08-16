f1a6e93e46e7c63a080d682ed7a8d2c4083eacf0
JungHyunLyoo <dkcmsadkcmsa@gmail.com>
2020-08-17 00:29:19 +0900

    modified dfs method

--- src/Q10/junghyunlyoo/Solution.java
+++ src/Q10/junghyunlyoo/Solution.java
@@ -7,30 +7,36 @@ package Q10.junghyunlyoo;
 
 public class Solution {
 
-    private void dfs(char[][] grid, int i, int j) {
+    private boolean isIsland(char[][] grid, int i, int j) {
         boolean isArrayEmpty = i < 0 || j < 0;
         boolean isArrayFullyChecked = i >= grid.length || j >= grid[0].length;
         if (isArrayEmpty || isArrayFullyChecked) {
-            return;
+            return false;
         }
-        boolean isLand = grid[i][j] != '1';
-        boolean isChecked = grid[i][j] == '#';
-        if (isLand || isChecked) {
+        return grid[i][j] == '1';
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (!isIsland(grid, i, j)) {
             return;
         }
 
-        grid[i][j] = '#';
+        grid[i][j] = '0';
 
-        dfs(grid, i-1, j);
-        dfs(grid, i+1, j);
-        dfs(grid, i, j-1);
-        dfs(grid, i, j+1);
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
     }
 
     public int numIslands(char[][] grid) {
+        int iLength;
+        int jLength;
+        if ((iLength = grid.length) == 0 ||
+                (jLength = grid[0].length) == 0) {
+            return 0;
+        }
         int islandsCount = 0;
-        int iLength = grid.length;
-        int jLength = grid[0].length;
         for (int i = 0; i < iLength; i++) {
             for (int j = 0; j < jLength; j++) {
                 if (grid[i][j] == '1') {
