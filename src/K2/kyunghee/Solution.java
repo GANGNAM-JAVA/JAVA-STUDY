9b25a74f714743e55eff39c8459b37be2f279d02
leekyunghee <qkqhlkh@gmail.com>
2020-08-30 23:49:08 +0900

    스택으로 크레인 인형뽑기 게임

---
+++ src/K2/kyunghee/Solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int[][] board, int[] moves) {
+
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for(Integer move : moves) {
+            int moveIndex = move -1;
+            for(int i = 0; i< board.length; i++) {
+                int k = board[i][moveIndex];
+                if(k == 0) continue;
+                if(!stack.empty() && stack.peek() == k) {
+                    stack.pop();
+                    answer++;
+                } else {
+                    stack.push(k);
+                }
+                board[i][moveIndex] = 0;
+                break;
+            }
+        }
+        return answer * 2;
+    }
+}
